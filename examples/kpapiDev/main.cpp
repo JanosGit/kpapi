bb0196f422416a427f777d050ea3dd8567c0babf
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2017-05-28 18:03:35 +0200

    Setup basic file structure

---
+++ examples/kpapiDev/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  kpapiDev
+//
+//  Created by Janos Buttgereit on 28.05.17.
+//  Copyright © 2017 Janos Buttgereit. All rights reserved.
+//
+
+#include <iostream>
+
+#include "../../kpapi.h"
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
---
+++ kpapi.h
@@ -0,0 +1,27 @@
+//
+//  kpapi.h
+//  
+//
+//  Created by Janos Buttgereit on 28.05.17.
+//
+//
+
+#ifndef kpapi_h
+#define kpapi_h
+
+#include "simpleMIDI/simpleMIDI.h"
+
+class KemperProfilingAmp {
+    
+public:
+    
+    
+    
+private:
+    
+    
+    
+};
+
+
+#endif /* kpapi_h */
