1d97896758eca144d992f05e4b2d18486d0fd2a4
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2018-02-01 15:28:10 +0100

    Starting to move stomp functionality in single source files
    Won't let the project get too messy

--- Stomps/StompBase.cpp
+++ Stomps/StompBase.cpp
@@ -12,24 +12,4 @@ void ProfilingAmp::StompBase::toggleOnOff (bool onOff) {
 
 
 
-bool ProfilingAmp::GenericWahStomp::isStillValid() {
-    if (((stompType & StompType::GenericMask) == StompType::GenericWah) && (_slotPage != PageUninitialized)) {
-        return true;
-    }
-    return false;
-}
 
-void ProfilingAmp::GenericWahStomp::setManual (uint16_t manual) {
-    _amp.updateHighResNRPN (_slotPage, WahManual, manual);
-}
-
-ProfilingAmp::GenericWahStomp* ProfilingAmp::getGenericWahStomp (StompSlot stompSlot) {
-    return (GenericWahStomp*) getGenericStompInstance (StompType::GenericWah, stompSlot);
-}
-
-bool ProfilingAmp::WahWahStomp::isStillValid() {
-    if (((stompType & StompType::SpecificMask) == StompType::WahWah) && (_slotPage != PageUninitialized)) {
-        return true;
-    }
-    return false;
-}
