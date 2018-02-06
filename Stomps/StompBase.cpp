18ff4f4e1f3cb26fac6349230d5377219305ac38
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2018-02-06 10:40:29 +0100

    Renaming the Stomp OnOff function

--- Stomps/StompBase.cpp
+++ Stomps/StompBase.cpp
@@ -4,12 +4,13 @@ ProfilingAmp::StompType ProfilingAmp::StompBase::getStompType() {
     return stompType;
 }
 
-void ProfilingAmp::StompBase::toggleOnOff (bool onOff) {
+void ProfilingAmp::StompBase::setToggleState (bool onOff) {
     _amp.updateLowResNRPN (_slotPage, NRPNParameter::OnOff, onOff);
 }
 
-
-
+bool ProfilingAmp::StompBase::getToggleState() {
+    return _amp.getSingleParameter (_slotPage, NRPNParameter::OnOff);
+}
 
 
 
